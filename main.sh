a361c8a0e8ae3bcf86af5a4d0e0cc26d279b8eac
hchenxa <chairmanhh1986@gmail.com>
2021-04-20 14:07:26 +0800

    generate report

--- main.sh
+++ main.sh
@@ -44,6 +44,7 @@ if [[ $USER_ENV == "true" ]]; then
 
    source utils/gen_report.sh
    generate_md results/${TIME_STAMP}/customer results/${TIME_STAMP}/customer/result.md $TEST_SNAPSHOT "test" "customer" "customer" "ClusterClaim" "ImportClusterClaim" hchentest
+   push_report results/${TIME_STAMP}/customer/result.md
 else
    supported_hub_type=$(jq -r ".acm_versions[]|select(.version == $ACM_VERSION)|.envs[].type" config/environment.json | xargs | sed 's/\ /,/g')
    echo "The supported hub type is $supported_hub_type in acm version $ACM_VERSION"
@@ -57,42 +58,43 @@ else
    # Filter the ACM type to run the cases on each of clusters.
    for type in ${type_array[@]}
    do
-       # (TODO) The url here may have multi value, need to handle this case later
-       url=$(jq -r ".acm_versions[]|select(.version == $ACM_VERSION) | .envs[] | select(.type == \"$type\") | .ocp_route" config/environment.json)
+        # (TODO) The url here may have multi value, need to handle this case later
+        url=$(jq -r ".acm_versions[]|select(.version == $ACM_VERSION) | .envs[] | select(.type == \"$type\") | .ocp_route" config/environment.json)
 
-       if [[ ! -z $OCP_TOKEN ]]; then
-           generate_context_withtoken $OCP_TOKEN $url $type $ACM_VERSION
-       else
-           # Get the username and password from the environment variable here, and the supported format should like AWS_23_USERNAME and AWS_23_PASSWORD
-           # (TODO) Will replace this part by using the 'gspreadsheets' API to get the username and password dynamicly
-           _username=${type}_${_version}_USERNAME
-           _passwd=${type}_${_version}_PASSWORD
+        if [[ ! -z $OCP_TOKEN ]]; then
+            generate_context_withtoken $OCP_TOKEN $url $type $ACM_VERSION
+        else
+            # Get the username and password from the environment variable here, and the supported format should like AWS_23_USERNAME and AWS_23_PASSWORD
+            # (TODO) Will replace this part by using the 'gspreadsheets' API to get the username and password dynamicly
+            _username=${type}_${_version}_USERNAME
+            _passwd=${type}_${_version}_PASSWORD
 
-           # (TODO) The url here may have multi value, need to handle this case later
-           generate_context $(eval echo '$'"$_username") $(eval echo '$'"$_passwd") $url $type $ACM_VERSION
-       fi
+            # (TODO) The url here may have multi value, need to handle this case later
+            generate_context $(eval echo '$'"$_username") $(eval echo '$'"$_passwd") $url $type $ACM_VERSION
+        fi
 
-       # Generate the imported cluster context
-       # First need to check to see if the cluster have imported cluster or not
-       # (TODO)Some cases required the managed cluster context, will handle this part later
-       _managed_cluster=$(get_imported_cluster "env_context/${type}_${ACM_VERSION}/kubeconfig")
-       if [[ $_managed_cluster == "" ]]; then
-           echo "no managed cluster in the cluster"
-       else
-           _imported_conf=$(generate_importcluster_context $_managed_cluster $type $ACM_VERSION)
-       fi
-       mkdir -p "results/${TIME_STAMP}/${type}_${ACM_VERSION}"
-       _base_domain=$(get_basedomain $type $ACM_VERSION)
-       _id_provider=$(get_idprovider $type $ACM_VERSION)
-       _config_path=$(get_config_path $type $ACM_VERSION)
+        # Generate the imported cluster context
+        # First need to check to see if the cluster have imported cluster or not
+        # (TODO)Some cases required the managed cluster context, will handle this part later
+        _managed_cluster=$(get_imported_cluster "env_context/${type}_${ACM_VERSION}/kubeconfig")
+        if [[ $_managed_cluster == "" ]]; then
+            echo "no managed cluster in the cluster"
+        else
+            _imported_conf=$(generate_importcluster_context $_managed_cluster $type $ACM_VERSION)
+        fi
+        mkdir -p "results/${TIME_STAMP}/${type}_${ACM_VERSION}"
+        _base_domain=$(get_basedomain $type $ACM_VERSION)
+        _id_provider=$(get_idprovider $type $ACM_VERSION)
+        _config_path=$(get_config_path $type $ACM_VERSION)
 
-       for tc in ${typecase_array[@]}
-       do
-           generate_options $_config_path $_base_domain $tc $(eval echo '$'"$_username") $(eval echo '$'"$_passwd") $_id_provider
-           run_test $tc $TIME_STAMP $type $ACM_VERSION
-       done
+        for tc in ${typecase_array[@]}
+        do
+            generate_options $_config_path $_base_domain $tc $(eval echo '$'"$_username") $(eval echo '$'"$_passwd") $_id_provider
+            run_test $tc $TIME_STAMP $type $ACM_VERSION
+        done
 
-       source utils/gen_report.sh
-       generate_md results/${TIME_STAMP}/${type}_${ACM_VERSION} results/${TIME_STAMP}/${type}_${ACM_VERSION}/result.md $TEST_SNAPSHOT "test" $type $ACM_VERSION "ClusterClaim" "ImportClusterClaim" hchentest
+        source utils/gen_report.sh
+        generate_md results/${TIME_STAMP}/${type}_${ACM_VERSION} results/${TIME_STAMP}/${type}_${ACM_VERSION}/result.md $TEST_SNAPSHOT "test" $type $ACM_VERSION "ClusterClaim" "ImportClusterClaim" hchentest
+        push_report results/${TIME_STAMP}/customer/result.md
    done
 fi
